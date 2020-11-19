87739f23a013c161940e40859492feae74a93fa3
chenjianfeng <1378022176@qq.com>
2020-11-19 14:24:42 +0800

    修改翻譯

--- task/antaranewsSpider.py
+++ task/antaranewsSpider.py
@@ -8,6 +8,8 @@
 # @Desc    : None
 import sys
 
+
+
 sys.path.append('..')
 import random
 import re
@@ -23,7 +25,7 @@ from utils.datautil import format_content_p, \
 from utils.timeUtil import now_datetime, now_datetime_no
 from configs.dbconfig import NewsTaskSql
 from utils.ossUtil import get_image, update_img
-from utils.youdaoutil import id_con_to_cn_con, youdao_id_en
+from utils.translate import translated_cn, en_con_to_cn_con
 
 
 # 安塔拉新闻 id
@@ -112,16 +114,16 @@ class AntaranewsSpider(object):
                 image_url = self.get_image_url(con)
                 caption = self.get_caption(html)
                 contents_html = self.get_content_html(con)
-                cn_title = youdao_id_en(title, 'id')
+                cn_title = translated_cn(title, 'id')
                 if not contents_html:
                     pass
                 else:
                     contents_html = subhead + self.get_content_html(con)
                     if caption:
-                        cn_caption = youdao_id_en(caption, 'id')
+                        cn_caption = translated_cn(caption, 'id')
                     else:
                         cn_caption = ""
-                    cn_content_ = id_con_to_cn_con(contents_html, 'id')
+                    cn_content_ = en_con_to_cn_con(contents_html, 'id')
                     if cn_content_:
                         if image_url:
                             ii = get_image(image_url)
